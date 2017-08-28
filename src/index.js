eee8f2a3231ca620e2435121fd319692b10f1ea4
SlazZy <slazzy.main@gmail.com>
2017-08-28 21:41:14 +0300

    fix errors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const hello = () => console.log('Welcome to the Brain Games!');
 
 const CORRECT_ANS_TO_WIN = 3;
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+
 const getName = () => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`\nHello, ${username}!`);
@@ -21,8 +23,23 @@ const brainEven = () => {
   hello();
   console.log('Answer "yes" if number even otherwise answer "no".');
   const username = getName();
+  let countCorrectAnswers = 0;
+  let question = 0;
 
-  const correctAnswers = 0;
+  while (countCorrectAnswers < CORRECT_ANS_TO_WIN) {
+    question = getRandomInt(1, 100);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${question}`);
+    const currentAnswer = getAns();
+
+    if (currentAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+      countCorrectAnswers += 1;
+    } else {
+      console.log(`'${currentAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${username}!`);
 };
 
 
