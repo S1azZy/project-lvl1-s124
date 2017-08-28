da42961453b482a24c0475d3c17ae4aab62ccb0a
Целовальников Артём Александрович <245@sfx-tula.ru>
2017-08-28 15:37:37 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const actual = readlineSync.question('May I have your name? ');
-console.log(`\nHello, ${actual}!`);
+const askQuestion = () => {
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`\nHello, ${actual}!`);
+};
