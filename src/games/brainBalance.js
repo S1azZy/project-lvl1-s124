4b9fb1f3427f18cd71c4acd20266419ed3a2fe37
Целовальников Артём Александрович <245@sfx-tula.ru>
2017-08-31 10:23:01 +0300

    add brainBalance game

--- src/games/brainBalance.js
+++ src/games/brainBalance.js
@@ -2,12 +2,38 @@ import { cons } from 'hexlet-pairs';
 import gameEngine from '..';
 import getRandomInt from '../tools/getRandomInt';
 
+const balance = (n) => {
+  const arr = Array.from(n.toString()).map(Number);
+
+  let goodEnough = false;
+  while (!goodEnough) {
+    let minIndex = 0;
+    let maxIndex = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+      if (arr[i] > arr[maxIndex]) {
+        maxIndex = i;
+      }
+    }
+    if (Math.abs(arr[maxIndex] - arr[minIndex]) <= 1) {
+      goodEnough = true;
+    }
+    if (!goodEnough) {
+      arr[maxIndex] -= 1;
+      arr[minIndex] += 1;
+    }
+  }
+  return arr.sort().join('');
+};
+
 export default () => {
   const startMassage = 'Balance the given number.\n';
   const rules = () => {
-    const num1 = getRandomInt(100, 9999);
-    const question = `${num1}`;
-    const correctAnswer = '';
+    const num = getRandomInt(100, 9999);
+    const question = `${num}`;
+    const correctAnswer = balance(num);
     return cons(question, correctAnswer);
   };
   gameEngine(startMassage, rules);
